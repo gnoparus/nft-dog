9822f8aaa09e4a1a43a2d1d2eb3f95a8358de012
gnoparus <surapong@gmail.com>
2022-02-16 12:25:58 +0700

    Created test for testnet.

---
+++ tests/integration/test_advanced_collectible_integration.py
@@ -0,0 +1,31 @@
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    fund_with_link,
+    get_account,
+    get_contract,
+    OPENSEA_URL,
+)
+from scripts.create_collectible_advance import create_collectible_advance
+from scripts.deploy_and_create_advance import deploy_and_create_advance
+from brownie import AdvancedCollectible, network, config
+from web3 import Web3
+import pytest
+import time
+
+
+def test_advanced_collectible_integration():
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    account = get_account()
+    advanced_collectible, creating_tx = deploy_and_create_advance()
+    ### emit requestCollectible(requestId, msg.sender)
+    time.sleep(60)
+
+    assert advanced_collectible.tokenCounter() == 1
+    # assert advanced_collectible.tokenIdToBreed(0) == STATIC_RNG % 3
+    assert advanced_collectible.ownerOf(0) == get_account()
+    assert advanced_collectible.balanceOf(get_account()) == 1
+
+
+def main():
+    test_advanced_collectible_integration()
