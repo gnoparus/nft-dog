1bfa5015ce830a09c43e267efe1f0bed04f2c2b5
gnoparus <surapong@gmail.com>
2022-02-16 11:15:53 +0700

    Created deploy script.

--- scripts/deploy_and_create_advance.py
+++ scripts/deploy_and_create_advance.py
@@ -1,12 +1,28 @@
-from scripts.helpful_scripts import get_account, OPENSEA_URL
-from brownie import AdvancedCollectible
-
-sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+from scripts.helpful_scripts import (
+    fund_with_link,
+    get_account,
+    get_contract,
+    OPENSEA_URL,
+)
+from brownie import AdvancedCollectible, network, config
 
 
 def deploy_and_create_advance():
     account = get_account()
-    advanced_collectible = AdvancedCollectible.deploy({"from": account})
+    advanced_collectible = AdvancedCollectible.deploy(
+        get_contract("vrf_coordinator"),
+        get_contract("link_token"),
+        config["networks"][network.show_active()]["key_hash"],
+        config["networks"][network.show_active()]["fee"],
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify", False),
+    )
+    print(f"Deployed AdvancedCollectible")
+    fund_with_link(advanced_collectible.address, account=account)
+    tx = advanced_collectible.createCollectible({"from": account})
+    tx.wait(1)
+    print(f"Created collectible")
+    return advanced_collectible
 
 
 def main():
