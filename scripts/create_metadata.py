dddf49391e2bbbd68a8d8d979da46355d2716dfa
gnoparus <surapong@gmail.com>
2022-02-16 15:31:25 +0700

    Finished upload image to ipfs.

--- scripts/create_metadata.py
+++ scripts/create_metadata.py
@@ -3,15 +3,55 @@ from scripts.helpful_scripts import (
     get_account,
     get_contract,
     OPENSEA_URL,
+    get_breed,
 )
 from brownie import AdvancedCollectible, network, config
 from web3 import Web3
+from metadata import sample_metadata
+from pathlib import Path
+from metadata.sample_metadata import metadata_template
+import requests
+
+
+def upload_to_ipfs(filepath):
+    with Path(filepath).open("rb") as fp:
+        image_binary = fp.read()
+        ipfs_url = "http://127.0.0.1:5001"
+        endpoint = "/api/v0/add"
+        response = requests.post(ipfs_url + endpoint, files={"file": image_binary})
+        ipfs_hash = response.json()["Hash"]
+        file_name = filepath.split("/")[-1:][0]
+        image_uri = f"https://ipfs.io/ipfs/{ipfs_hash}?filename={file_name}"
+        print(f"image_uri = {image_uri}")
+        return image_uri
 
 
 def create_metadata():
     advanced_collectible = AdvancedCollectible[-1]
     number_of_advanced_collectibles = advanced_collectible.tokenCounter()
     print(f"You've created {number_of_advanced_collectibles} collectible(s)")
+
+    for token_id in range(number_of_advanced_collectibles):
+        breed = get_breed(advanced_collectible.tokenIdToBreed(token_id))
+        metadata_file_name = (
+            f"./metadata/{network.show_active()}/{token_id}-{breed}.json"
+        )
+        print(f"metada_file_name = {metadata_file_name}")
+
+        collectible_metada = metadata_template
+        if Path(metadata_file_name).exists():
+            print(f"{metadata_file_name} is already exists. Delete it to overwrite.")
+        else:
+            print(f"Creating metadata file {metadata_file_name}")
+
+            collectible_metada["name"] = breed
+            collectible_metada["description"] = f"Adoreble breed of {breed}"
+            collectible_metada["image_uri"] = ""
+            # print(f"collectible_metada = {collectible_metada}")
+            image_file_name = "./img/" + breed.lower().replace("_", "-") + ".png"
+            upload_to_ipfs(image_file_name)
+
+        # advanced_collectible.setTokenURI(token_id, "")
 
 
 def main():
